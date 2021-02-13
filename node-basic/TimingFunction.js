d34573cd13ea36af4407d70511ee06c7e5a90d70
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2021-02-13 13:03:25 +0700

    node-basic

---
+++ node-basic/TimingFunction.js
@@ -0,0 +1,6 @@
+// setInterval, bisa dilakukan untuk waktu tertentu secara berulang
+
+// setTimeout, akan dilakukan (menunggu dijalankan) dengan waktu sudah dtentukan, dan habis jika waktunya sudah habis
+setTimeout(function () {
+  console.log("Hello Node JS");
+}, 3000); // nunggu 3 detik
---
+++ node-basic/latihan1.js
@@ -0,0 +1,3 @@
+// ada GLOBAL OBJECT yakni __filename
+console.log(__filename); // hasilnya adalah nama file yang dibuat
+console.log(__dirname); // hasilnya adalah directory dari file yang dibuat
